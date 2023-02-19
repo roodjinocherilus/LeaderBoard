4cb062fa348f14c55e60b67a485aafbfce4a9e73
Roodjino Cherilus <cherilusjcr@gmail.com>
2023-02-18 23:30:57 -0500

    fix linters

--- src/index.js
+++ src/index.js
@@ -4,22 +4,19 @@ import './style.css';
 import scores from './modules/get/score';
 import addScore from './modules/post/score';
 
-
 const scoresList = document.querySelector('.list');
 const submitBtn = document.querySelector('.submit');
 const refreshBtn = document.querySelector('.refresh');
 const prompt = document.querySelector('.prompt');
 
-
-
 refreshBtn.addEventListener('click', (e) => {
-    e.preventDefault();
-    scores(scoresList);
-  });
-  
-  submitBtn.addEventListener('click', (e) => {
-    e.preventDefault();
-    const user = document.querySelector('.user').value;
-    const score = document.querySelector('.score').value;
-    addScore(scoresList, user, score, prompt);
-  });
+  e.preventDefault();
+  scores(scoresList);
+});
+
+submitBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  const user = document.querySelector('.user').value;
+  const score = document.querySelector('.score').value;
+  addScore(scoresList, user, score, prompt);
+});
--- src/modules/post/score.js
+++ src/modules/post/score.js
@@ -1,32 +1,31 @@
-import showPrompt from "../get/prompt.js";
-import scores from "../get/score.js";
-
+import showPrompt from '../get/prompt.js';
+import scores from '../get/score.js';
 
 const postScore = async (scores, user, score, message) => {
-    if (user === '' || score === '') {
-      showPrompt(message, 'success', 'error', 'Please fill in all fields');
+  if (user === '' || score === '') {
+    showPrompt(message, 'success', 'error', 'Please fill in all fields');
+  } else {
+    const response = await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/hjQ1d7tYqYfYNIn76Anu/scores/', {
+      method: 'POST',
+      body: JSON.stringify({
+        user,
+        score,
+      }),
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+    });
+    if (response.ok) {
+      scores(scores);
     } else {
-      const response = await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/hjQ1d7tYqYfYNIn76Anu/scores/', {
-        method: 'POST',
-        body: JSON.stringify({
-          user,
-          score,
-        }),
-        headers: {
-          'Content-type': 'application/json; charset=UTF-8',
-        },
-      });
-      if (response.ok) {
-        scores(scores);
-      } else {
-        showPrompt(message, 'success', 'error', 'Something went wrong');
-      }
-      document.querySelector('.user').value = '';
-      document.querySelector('.score').value = '';
-  
-      const json = await response.json();
-      showPrompt(message, 'error', 'success', json.result);
+      showPrompt(message, 'success', 'error', 'Something went wrong');
     }
-  };
+    document.querySelector('.user').value = '';
+    document.querySelector('.score').value = '';
 
-  export default postScore;
+    const json = await response.json();
+    showPrompt(message, 'error', 'success', json.result);
+  }
+};
+
+export default postScore;
